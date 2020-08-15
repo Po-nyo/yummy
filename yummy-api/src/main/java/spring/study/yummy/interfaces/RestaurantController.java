68b47f294dad647ff0dfd39d160fb16ff13d1c86
WooKyungjin <wkj1103@naver.com>
2020-08-16 03:02:56 +0900

    Validation check

--- yummy-api/src/main/java/spring/study/yummy/interfaces/RestaurantController.java
+++ yummy-api/src/main/java/spring/study/yummy/interfaces/RestaurantController.java
@@ -2,10 +2,12 @@ package spring.study.yummy.interfaces;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import spring.study.yummy.application.RestaurantService;
 import spring.study.yummy.domain.Restaurant;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
@@ -32,7 +34,7 @@ public class RestaurantController {
     }
 
     @PostMapping("/restaurants")
-    public ResponseEntity<?> create(@RequestBody Restaurant resource) throws URISyntaxException {
+    public ResponseEntity<?> create(@Valid @RequestBody Restaurant resource) throws URISyntaxException {
         Restaurant restaurant = restaurantService.addRestaurant(Restaurant.builder()
                 .name(resource.getName())
                 .address(resource.getAddress())
@@ -43,7 +45,7 @@ public class RestaurantController {
     }
 
     @PatchMapping("restaurants/{id}")
-    public String update(@PathVariable("id") Long id, @RequestBody Restaurant resource) {
+    public String update(@PathVariable("id") Long id, @Valid @RequestBody Restaurant resource) {
         String name = resource.getName();
         String address = resource.getAddress();
         restaurantService.updateRestaurant(id, name, address);
