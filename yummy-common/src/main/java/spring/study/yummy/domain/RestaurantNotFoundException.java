7dd50247e5070fd9f539d812865c404c10837f6d
WooKyungjin <wkj1103@naver.com>
2020-08-17 21:17:57 +0900

    fixed untracked files

--- yummy-common/src/main/java/spring/study/yummy/domain/RestaurantNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package spring.study.yummy.domain;
-
-public class RestaurantNotFoundException extends RuntimeException {
-
-    public RestaurantNotFoundException(Long id) {
-        super("Could not find restaurant id : " + id);
-    }
-}
